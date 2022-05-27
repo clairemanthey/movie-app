22e2eca1df546ae2c195f6caae1a3bff9bc0dd74
Claire <63174119+clairemanthey@users.noreply.github.com>
2022-05-27 16:46:13 -0500

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React from 'react';
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import { GlobalStyle } from "./GlobalStyles"
-
-import Header from "./components/Header";
-import Home from "./components/Home";
-import Movie from "./components/Movie";
-import NotFound from "./components/NotFound";
+// Routing
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// Components
+import Header from './components/Header';
+import Home from './components/Home';
+import Movie from './components/Movie';
+import NotFound from './components/NotFound';
+import Login from './components/Login';
+// Context
+import UserProvider from './context';
+// Styles
+import { GlobalStyle } from './GlobalStyles';
 
 const App = () => (
-    <Router className="App">
+  <Router>
+    <UserProvider>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/:movieId" element={<Movie />} /> 
-        <Route path="/*" element={<NotFound />} />
-      </Routes>  
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/:movieId' element={<Movie />} />
+        <Route path='/*' element={<NotFound />} />
+      </Routes>
       <GlobalStyle />
-    </Router>
+    </UserProvider>
+  </Router>
 );
-
 
 export default App;
