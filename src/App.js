430e9e146678ca6dae4300bc9589b3d02aaaf618
Claire <63174119+clairemanthey@users.noreply.github.com>
2022-08-09 17:59:49 -0500

    Updated App.js
    deleted Login, was not being used

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Header from './components/Header';
 import Home from './components/Home';
 import Movie from './components/Movie';
 import NotFound from './components/NotFound';
-import Login from './components/Login';
 // Context
 import UserProvider from './context';
 // Styles
